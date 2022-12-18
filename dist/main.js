d34afe4160afec334506a81a3f90c489f7e078e5
Stanley <Stanley3@Stanleys-MacBook-Pro.local>
2022-12-17 20:26:29 -0600

    Vite config

--- dist/main.js
+++ dist/main.js
@@ -1,8 +1,9 @@
 import './style.css'
 import * as THREE from 'three';
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
-//import { isAbsolute } from 'path';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';//import { isAbsolute } from 'path';
 //import { request } from 'http';
+
+
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1,1000);
